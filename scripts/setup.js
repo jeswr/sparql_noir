4368fbbfdd59d936644fdfebaca56eaf042bc0cb
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-09-02 18:38:41 +0100

    WIP: refactort

---
+++ scripts/setup.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import mappings from '../mappings.json' with { type: 'json' };
+import { defaultConfig } from '../dist/config.js';
+const { noir, rename } = mappings;
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+for (const file of fs.readdirSync(path.join(__dirname, '..', 'noir'), { recursive: true })) {
+  if (file.endsWith('.nr.template')) {
+    let libTemplate = fs.readFileSync(path.join(__dirname, '..', 'noir', file), 'utf8');
+
+    for (const [key, value] of Object.entries(noir))
+      libTemplate = libTemplate.replace(`{{${key}}}`, value[defaultConfig[rename[key] ?? key]]);
+
+    fs.writeFileSync(path.join(__dirname, '..', 'noir', file.replace('.nr.template', '.nr')), libTemplate);
+  }
+}
--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ interface IConfig {
 }
 
 export const defaultConfig: IConfig = {
-  stringHash: 'blake3',
+  stringHash: 'sha256',
   fieldHash: 'pedersen',
   merkleDepth: 11,
   signature: 'secp256k1',
